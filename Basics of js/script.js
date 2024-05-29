5145b9ab5eed0b236c5278d24c04c4928327c4a9
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-05-29 10:37:57 +0530

    Updated

---
+++ Basics of js/script.js
@@ -0,0 +1,18 @@
+// alert (" Hello Backend");
+
+let arr = [1,2,3,4,5]
+
+// FOREACH
+
+arr.forEach(function(value){
+    console.log(value)
+})
+arr.forEach((v)=>{
+    console.log(v+66)
+})
+
+// MAP 🤖
+
+arr.map((value)=>{
+  console.log(value +12)
+})
