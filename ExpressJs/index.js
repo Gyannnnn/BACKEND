fa7aee8a4ec9e74a42838a5bc7cbfa101067cccd
Gyanranjan Patra <hi.gyanaranjanpatra@gmail.com>
2024-05-30 15:29:39 +0530

    l

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const app = express()
+app.use(function(req,res,next){
+     console.log("Middleware Is Running !!!!");
+     next();
+});
+app.use(function(req,res,next){
+   console.log("Middle ware Is working fine");
+   next();                                                                                                                                                                                                                                              
+   
+})
 app.get("/",function(req,res){
-    res.end("Welcome To Home");
+    res.send("Welcome To Home");
 })
 app.get("/Profile",function(req,res){
-    res.end("Welcome To My Profile");
+    res.send("Welcome To My Profile");
 })
-app.get("/Profile/User",function(req,res){
-    res.end("Name:Gyanaranjan Patra Age :20");
-})
-app.get("/contact",function(req,res){
-    res.end("Phone- 7978648082")
-})
-// app.get("/contact/wp",function(req,res){
-//     res.end("Phone- 7978648082")
+// app.get("/Profile/User",function(req,res){
+//     res.send("Name:Gyanaranjan Patra Age :20");
 // })
-app.get("/contact/wp",function(req,res){
-    res.end("WhatsApp no : 7978648082")
-})
-app.get("/contact/instagram",function(req,res){
-    res.end("id: gyan.css")
-})
+// app.get("/contact",function(req,res){
+//     res.send("Phone- 7978648082")
+// })
+
+// app.get("/contact/wp",function(req,res){
+//     res.send("WhatsApp no : 7978648082")
+// })
+// app.get("/contact/instagram",function(req,res){
+//     res.send("id: gyan.css")
+// })
 
 app.listen(9000)
