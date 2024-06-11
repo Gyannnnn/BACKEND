f40a1a2a17d7c99604f3348500ba65f56fbbc324
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-06-11 12:28:54 +0530

    Authentication Completed

--- AuthenticationAuthorisation/app.js
+++ AuthenticationAuthorisation/app.js
@@ -28,33 +28,65 @@
 
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const app  = express();
+const app = express();
 const path = require('path');
-const  userModel = require('../AuthenticationAuthorisation/models/user');
+const userModel = require('../AuthenticationAuthorisation/models/user');
 const { model } = require('mongoose');
+const bcrypt = require('bcrypt');
+const { hash } = require('crypto');
+const jwt = require('jsonwebtoken');
+const user = require('../AuthenticationAuthorisation/models/user');
 
-
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser())
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('index')
 
 })
-app.post("/create",async(req,res)=>{
-    let {username,email,passward,age} = req.body
-    let createdUser =    await userModel.create({
-        username,
-        email,
-        passward,  
-        age              
+app.post("/create",  (req, res) => {
+    let { username, email, passward, age } = req.body
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(passward, salt, async(err, hash) => {
+            let createdUser = await userModel.create({
+                username,
+                email,
+                passward:hash,
+                age
+            })
+            let token  = jwt.sign({email},'gyan');
+            res.cookie('token',token);
+            res.send(createdUser)
+        })
     })
-    res.send(createdUser)
+
+    
+})
+app.get("/logout",(req,res)=>{
+    res.cookie("token","");
+    res.redirect("/");
+})
+app.get("/login",(req,res)=>{
+    res.render('login')
+});
+app.post('/login',async(req,res)=>{
+    let logedUser  = await userModel.findOne({email:req.body.email})
+    if(!logedUser) return res.send("Something Went Wrong");
+
+    bcrypt.compare(req.body.passward,user.passward,(err,result)=>{
+        if(result){
+            let token  = jwt.sign({email:user.email},"gyan");
+            res.cookie('token',token)
+            res.send("Yus You can login");
+
+        }
+        else res.send("Something is Wrong")
+    })
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Working excelent")
 })
